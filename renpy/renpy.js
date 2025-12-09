98a3f8a57f69d11c4e101b913a3c4783b72e4485
_ZiolKen <0022446688d@gmail.com>
2025-12-09 08:44:14 +0700

    Update renpy.js

--- renpy/renpy.js
+++ renpy/renpy.js
@@ -346,7 +346,7 @@
               ]
             };
 
-            const response = await fetch('https://api.deepseek.com/v1/chat/completions', {
+            const response = await fetch('https://api.deepseek.com/chat/completions', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
